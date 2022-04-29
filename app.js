c3c9dac79a1af4be815ea9d8720dccc28b5a5770
sjapaget <sjapaget@gmail.com>
2022-04-29 23:16:24 +0200

    reworks: media queries tablet & navtoggle button

--- app.js
+++ app.js
@@ -1,4 +1,22 @@
+/* const navButton = document.getElementById("nav-menu-button");
+const nav = document.querySelector(".navbar");
 
+navButton.onclick = function() {
+   
+    if (nav.style.display == "none") {
+        navButton.classList.remove("fa-bars");
+        navButton.classList.add("fa-xmark");
+        nav.style.display = "flex";
+    } else {
+        navButton.classList.remove("fa-xmark");
+        navButton.classList.add("fa-bars");
+        nav.style.display = "none";
+    }
+
+};
+
+*/
+const navBar = document.querySelector(".navbar")
 const navToggleButton = document.querySelector("#nav-menu-button");
 const navOptions = document.querySelector("#nav-options");
 const navHeader = document.querySelector("#navbar-header");
@@ -10,11 +28,17 @@ navToggleButton.onclick = function() {
     
     if (unclicked) {
         unclicked = false;
+        navToggleButton.classList.remove("fa-bars");
+        navToggleButton.classList.add("fa-xmark");
+        navBar.style.backgroundColor = "#d6c6ad"
         navHeader.style.display = "none"
         navLogo.style.display = "none"
         navOptions.style.display = "flex";
     } else {
         unclicked = true;
+        navToggleButton.classList.remove("fa-xmark");
+        navToggleButton.classList.add("fa-bars");
+        navBar.style.backgroundColor = "#EDE6DB"
         navHeader.style.display = "flex";
         navLogo.style.display = "flex";
         navOptions.style.display = "none";
